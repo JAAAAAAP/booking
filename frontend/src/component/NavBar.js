0009a1198d54c970de64f41bc953878425169509
unknown <jr8o6ez@gmail.com>
2024-03-12 15:31:04 +0700

    สร้างหน้าเว็บ01

---
+++ frontend/src/component/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+    return (
+        <nav>
+        <div className="logo">photo</div>
+        <ul>
+            <li><a href="">เข้าสู่ระบบ</a></li>
+            <li><a href="">ติดต่อ</a></li>
+        </ul>
+    </nav>
+    );
+}
+export default NavBar;
